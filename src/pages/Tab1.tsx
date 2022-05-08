3c913a5f6357d222662a3ed414cf287be65b5e68
João <j.henr@outlook.com>
2022-05-08 15:06:55 -0300

    first commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,22 +1,66 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  useIonAlert,
+} from "@ionic/react";
+import { useState } from "react";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
+  const [name, setName] = useState<string>();
+  const [registration, setRegistration] = useState<string>();
+  const [present] = useIonAlert();
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle size="small">IFPI - Instituto Federal do Piauí</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <h1>Cadastro de Alunos</h1>
+        <IonItem>
+          <IonLabel position="floating">Nome</IonLabel>
+          <IonInput
+            type="text"
+            value={name}
+            onIonChange={(e) => setName(e.detail.value!)}
+          ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Matrícula</IonLabel>
+          <IonInput
+            type="number"
+            value={registration}
+            onIonChange={(e) => setRegistration(e.detail.value!)}
+          ></IonInput>
+        </IonItem>
+        <IonButton
+          expand="block"
+          shape="round"
+          type="submit"
+          onClick={() =>
+            present({
+              cssClass: "my-css",
+              header: "Alert",
+              message: `Nome: ${name}\nMatrícula: ${registration}`,
+              buttons: [
+                "Cancel",
+                { text: "Ok", handler: (d) => console.log("ok pressed") },
+              ],
+              onDidDismiss: (e) => console.log("did dismiss"),
+            })
+          }
+        >
+          Cadastrar
+        </IonButton>
       </IonContent>
     </IonPage>
   );
